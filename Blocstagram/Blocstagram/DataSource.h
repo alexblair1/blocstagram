302a4c93c6c6574522dbf5bc0a1aaf56c0a8c94e
FirstName LastName <alexblair190@gmail.com>
2015-05-30 17:40:29 -0600

    checkpoint 32 help request

--- Blocstagram/Blocstagram/DataSource.h
+++ Blocstagram/Blocstagram/DataSource.h
@@ -10,12 +10,21 @@
 
 @class Media;
 
+typedef void (^NewItemCompletionBlock)(NSError *error);
+
 @interface DataSource : NSObject
 
 +(instancetype) sharedInstance;
 
 @property (nonatomic, strong, readonly) NSArray *mediaItems;
+@property (nonatomic, strong, readonly) NSString *accessToken;
 
 - (void) deleteMediaItem: (Media *) item;
+
+- (void) requestNewItemsWithCompletionHandler:(NewItemCompletionBlock) completionHandler;
+
+- (void) requestOldItemsWithCompletionHandler:(NewItemCompletionBlock) completionHandler;
+
++ (NSString *) instagramClientID;
 
 @end
--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -19,6 +19,23 @@
 
 @implementation ImagesTableViewController
 
+- (void) infiniteScrollIfNecessary {
+    // #3
+    NSIndexPath *bottomIndexPath = [[self.tableView indexPathsForVisibleRows] lastObject];
+    
+    if (bottomIndexPath && bottomIndexPath.row == [DataSource sharedInstance].mediaItems.count - 1) {
+        // The very last cell is on screen
+        [[DataSource sharedInstance] requestOldItemsWithCompletionHandler:nil];
+    }
+}
+
+#pragma mark - UIScrollViewDelegate
+
+// #4
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    [self infiniteScrollIfNecessary];
+}
+
 -(NSArray *) items {
     return [DataSource sharedInstance].mediaItems;
 }
@@ -28,6 +45,9 @@
     
     [[DataSource sharedInstance] addObserver:self forKeyPath:@"mediaItems" options:0 context:nil];
     
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(refreshControlDidFire:) forControlEvents:UIControlEventValueChanged];
+    
     [self.tableView registerClass:[MediaTableViewCell class] forCellReuseIdentifier:@"mediaCell"];
     
     // Uncomment the following line to preserve selection between presentations.
@@ -36,6 +56,12 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
 //       self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
+
+- (void) refreshControlDidFire:(UIRefreshControl *) sender {
+    [[DataSource sharedInstance] requestNewItemsWithCompletionHandler:^(NSError *error) {
+        [sender endRefreshing];
+    }];
+    }
 
 -(void) dealloc {
     [[DataSource sharedInstance] removeObserver:self forKeyPath:@"mediaItems"];
